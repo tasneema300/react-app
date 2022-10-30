07b692cf5006f6a67da2317a857d154f500b4550
Tasneem Abdalla <tasneemabdalla@macbook-pro-52.ads.northwestern.edu>
2022-10-30 14:57:14 -0500

    fetched data

--- Desktop/NUDM/nudm-app/src/App.js
+++ Desktop/NUDM/nudm-app/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './App.css';
 
 const schedule = {
   "title": "CS Courses for 2018-2019",
@@ -26,20 +27,37 @@ const schedule = {
   }
 };
 
-const App = () =>  (
-  <div>
-    <Banner title={ schedule.title } />
-    <CourseList courses={ schedule.courses } />
-  </div>
-);
+const App = () => {
+  const [schedule, setSchedule] = useState();
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, []);
+
+  if (!schedule) return <h1>Loading schedule...</h1>;
+
+  return (
+    <div className="container">
+      <Banner title={ schedule.title } />
+      <CourseList courses={ schedule.courses } />
+    </div>
+  );
+};
 
 const Banner = ({ title }) => (
     <h1>{ title }</h1>
   );
 
 const CourseList = ({ courses }) => (
-  <div>
-    { Object.values(courses).map(course => <Course key={course.id} course={ course } />) }
+  <div className="course-list">
+  { Object.values(courses).map(course => <Course key={course.id} course={ course } />) }
   </div>
 );
 
@@ -54,8 +72,11 @@ const getCourseNumber = course => (
 );
 
 const Course = ({ course }) => (
-  <div>
-    { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
+  <div className="card m-4 p-3" >
+    <div className="card-body">
+      <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
+      <div className="card-text">{ course.title }</div>
+    </div>
   </div>
 );
 
